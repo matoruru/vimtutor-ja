6c96c229d36ba68b38682955d5489399d35791d1
matoruru <matoruru@shgt.jp>
2019-04-02 10:22:14 +0900

    modify

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,4 @@
 rm -rf ~/.local/share/matoruru/vimtutor-ja
 rm -rf ~/.cache/matoruru/vimtutor-ja
-rm /usr/local/bin/vimtutor-ja
+sudo rm /usr/local/bin/vimtutor-ja
+rm -rf ~/.vim/matoruru/vimtutor-ja
